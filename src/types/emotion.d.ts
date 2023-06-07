dbc44ae56cd6c8b0a51c347d26d481e52efc34e7
ohtaekwon <ohtk1119@gmail.com>
2023-06-07 21:53:56 +0900

    Chore : Add Theme - color, weight, linhieght

--- src/types/emotion.d.ts
+++ src/types/emotion.d.ts
@@ -4,6 +4,7 @@ declare module "@emotion/react" {
   export interface Theme {
     colors: typeof theme.colors;
     fontSize: typeof theme.fontSize;
+    fontWeight: typeof theme.fontWeight;
     lineHeight: typeof theme.lineHeight;
     breakpoint: typeof theme.breakpoint;
     mediaQuery: typeof theme.mediaQuery;
