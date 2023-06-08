9637e9cdb3b7e3dbdeabe1bee67736c34170a10b
ohtaekwon <ohtk1119@gmail.com>
2023-06-09 00:28:31 +0900

    Feat : members & non-members's axios module and user's rest api

---
+++ src/api/clients/public.client.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import queryString from "query-string";
+
+const baseURL = process.env.REACT_APP_BASE_PATH;
+
+const publicClient = axios.create({
+  baseURL,
+  paramsSerializer: {
+    encode: (params) => queryString.stringify(params),
+  },
+});
+publicClient.interceptors.request.use(async (config) => {
+  const headers = {
+    "Content-type": "application/json;charset=UTF-8",
+  };
+  return {
+    ...config,
+    ...headers,
+  };
+});
+publicClient.interceptors.response.use(
+  (response) => {
+    // 응답이 성공적으로 처리된 경우
+    if (response && response.data) {
+      return response.data;
+    }
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default publicClient;
