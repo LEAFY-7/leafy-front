7a41dbd01276404ed71e9979a6f4ecb177b5b312
ohtaekwon <ohtk1119@gmail.com>
2023-06-12 02:12:11 +0900

    Feat : Add PrivateRotuer for login member

---
+++ src/components/PrivateRoute.tsx
@@ -0,0 +1,41 @@
+import { RootState } from "@redux/store";
+import { useSelector } from "react-redux";
+import { Route, Navigate } from "react-router-dom";
+
+// 인증 상태를 확인하는 함수
+const isAuthenticated = (user: any): boolean => {
+  // 로그인 상태를 확인하고 true 또는 false를 반환
+  if (!user) false;
+  return true;
+};
+
+// PrivateRoute 컴포넌트
+interface PrivateRouteProps {
+  path: string;
+  component: React.ComponentType;
+}
+
+const PrivateRoute: React.FC<PrivateRouteProps> = ({
+  component: Component,
+  ...rest
+}) => {
+  const { user } = useSelector((state: RootState) => state.user);
+
+  return (
+    <Route
+      {...rest}
+      element={
+        isAuthenticated(user) ? (
+          <Component />
+        ) : (
+          <Navigate
+            to="/login"
+            replace
+            state={{ from: window.location.pathname }}
+          />
+        )
+      }
+    />
+  );
+};
+export default PrivateRoute;
