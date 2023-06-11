f0e2908fa3af798df44e918e09e70ded0e6b1b85
ohtaekwon <ohtk1119@gmail.com>
2023-06-12 02:13:56 +0900

    Chore : Change router code and Add universial-cookie

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Global, ThemeProvider } from "@emotion/react";
 import { ToastContainer } from "react-toastify";
 
 import theme from "@styles/theme";
 import globalStyle from "@styles/globalStyle";
-import MainLayout from "@components/layout/MainLayout";
-import routes from "@routes/router";
-import PageWrapper from "@components/PageWrapper";
+import Router from "@routes/router";
 
 import "react-toastify/dist/ReactToastify.css";
 
@@ -25,41 +23,7 @@ function App() {
       />
       <ThemeProvider theme={theme}>
         <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<MainLayout />}>
-              {routes.map((route, index) =>
-                route.index ? (
-                  <Route
-                    index
-                    key={index}
-                    element={
-                      route.state ? (
-                        <PageWrapper state={route.state}>
-                          {route.element}
-                        </PageWrapper>
-                      ) : (
-                        route.element
-                      )
-                    }
-                  />
-                ) : (
-                  <Route
-                    path={route.path}
-                    key={index}
-                    element={
-                      route.state ? (
-                        <PageWrapper state={route.state}>
-                          {route.element}
-                        </PageWrapper>
-                      ) : (
-                        route.element
-                      )
-                    }
-                  />
-                )
-              )}
-            </Route>
-          </Routes>
+          <Router />
         </BrowserRouter>
         <Global styles={globalStyle} />
       </ThemeProvider>
