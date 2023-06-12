f4bdc50bf469fabd26b109e94066206b6c452fd6
ohtaekwon <ohtk1119@gmail.com>
2023-06-12 15:40:05 +0900

    Chore : Modify router according to status

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import Home from "@pages/Home/Page";
 import FeedDetail from "@pages/FeedDetail/Page";
@@ -16,6 +16,8 @@ import Setting from "@pages/Setting/Page";
 import Search from "@pages/Search/Page";
 import PrivateRoute from "@components/PrivateRoute";
 import MainLayout from "@components/layout/MainLayout";
+import Unauthorized from "@components/Unauthorized";
+import Qna from "@pages/QnaDetail/Page";
 
 export const routesGen = {
   home: "/",
@@ -33,77 +35,82 @@ export const routesGen = {
   chat: "/chat",
 };
 
+const STATUS = {
+  ADMIN: "admin",
+  NORMAL: "normal",
+  MEMBER: "member",
+};
+
 const Router = () => {
   return (
-    <>
-      <Routes>
-        <Route element={<MainLayout />}>
-          {/* ------ 일반 라우터 ------ */}
+    <Routes>
+      <Route element={<MainLayout />}>
+        {/* 메인 피드 페이지 */}
+        <Route index path="/" element={<Home />} />
+        {/* 메인 피드 페이지 */}
 
-          {/* 메인 피드 페이지 */}
-          <Route index path="/" element={<Home />} />
-          {/* 메인 피드 페이지 */}
+        {/* 피드 상세 페이지 */}
+        <Route path=":feedId" element={<FeedDetail />} />
+        {/* 피드 상세 페이지 */}
 
-          {/* 피드 상세 페이지 */}
-          <Route path="/:feedId" element={<FeedDetail />} />
-          {/* 피드 상세 페이지 */}
+        {/* 로그인/회원가입 페이지 */}
+        <Route path="auth" element={<Auth />} />
+        {/* 로그인/회원가입 페이지 */}
 
-          {/* 로그인/회원가입 페이지 */}
-          <Route path="/auth" element={<Auth />} />
-          {/* 로그인/회원가입 페이지 */}
+        {/* 유저 피드 페이지 */}
+        <Route path="user/:userId" element={<User />} />
+        {/* 유저 피드 페이지 */}
 
-          {/* 유저 피드 페이지 */}
-          <Route path="/user/:userId" element={<User />} />
-          {/* 유저 피드 페이지 */}
+        {/* 검색 결과 페이지 */}
+        <Route path="search" element={<Search />} />
+        {/* 검색 결과 페이지 */}
 
-          {/* 검색 결과 페이지 */}
-          <Route path="/search" element={<Search />} />
-          {/* 검색 결과 페이지 */}
+        {/* 공지사항 페이지 */}
+        <Route path="notice" element={<Notice />} />
+        {/* 공지사항 페이지 */}
 
-          {/* 공지사항 페이지 */}
-          <Route path="/notice" element={<Notice />} />
-          {/* 공지사항 페이지 */}
+        {/* 공지사항 상세 페이지 */}
+        <Route path="notice/:noticeId" element={<NoticeDetail />} />
+        {/* 공지사항 상세 페이지 */}
 
-          {/* 공지사항 상세 페이지 */}
-          <Route path="/notice/:noticeId" element={<NoticeDetail />} />
-          {/* 공지사항 상세 페이지 */}
+        <Route path="unauthorized" element={<Unauthorized />} />
 
-          {/* ------ 일반 라우터 ------ */}
+        {/* 404 페이지 */}
+        <Route path="/*" element={<NotFound />} />
+        {/* 404 페이지 */}
 
-          {/* 404 페이지 */}
-          <Route path="/*" element={<NotFound />} />
-          {/* 404 페이지 */}
-
-          {/* ------ 로그인 라우터 ------ */}
-
+        <Route element={<PrivateRoute allowedRoles={["member", "admin"]} />}>
           {/* // 피드 작성 페이지 */}
-          <PrivateRoute path="/post" component={FeedPost} />
+          <Route path="post" element={<FeedPost />} />
           {/* // 피드 작성 페이지 */}
 
           {/*  피드 수정 페이지 */}
-          <PrivateRoute path="/:feedId/update" component={FeedUpdate} />
+          <Route path=":feedId/update" element={<FeedUpdate />} />
           {/*  피드 수정 페이지 */}
 
           {/* 팔로우 페이지 */}
-          <PrivateRoute path="/follow" component={Follow} />
+          <Route path="follow" element={<Follow />} />
           {/* 팔로우 페이지 */}
 
           {/* 채팅 페이지 */}
-          <PrivateRoute path="/chat" component={Chat} />
+          <Route path="chat" element={<Chat />} />
           {/* 채팅 페이지 */}
 
           {/* 회원 정보 페이지 */}
-          <PrivateRoute path="/mypage" component={MyPage} />
+
+          <Route path="mypage" element={<MyPage />} />
           {/* 회원 정보 페이지 */}
 
           {/* 설정 페이지 */}
-          <PrivateRoute path="/mypage/setting" component={Setting} />
+          <Route path="mypage/setting" element={<Setting />} />
           {/* 설정 페이지 */}
 
-          {/* ------ 로그인 라우터 ------ */}
+          {/* Qna 상세 페이지 */}
+          <Route path="notice/qna/:qnaId" element={<Qna />} />
+          {/* Qna 상세페이지 */}
         </Route>
-      </Routes>
-    </>
+      </Route>
+    </Routes>
   );
 };
 export default Router;
