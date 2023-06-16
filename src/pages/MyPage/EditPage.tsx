725715ff7e47e1dfc4840472898e6177b5ef93d9
ohtaekwon <ohtk1119@gmail.com>
2023-06-16 20:41:05 +0900

    [ADD] 회원정보 페이지 및 필수 입력 컴포넌트 구현

---
+++ src/pages/MyPage/EditPage.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Box from "@components/ui/Box";
+import Flex from "@components/ui/Flex";
+import Typography from "@components/ui/Typograph";
+import Container from "@components/Container";
+import EditPageRequire from "./EditPageRequire";
+import EditPageAdditional from "./EditPageAdditional";
+
+const EditPage = () => {
+  return (
+    <Flex as="div" direction="column">
+      <Box
+        as="section"
+        width={"1000px"}
+        overflow="hidden"
+        display="flex"
+        direction="column"
+      >
+        <Typography variant="H2" marginTop={5} marginBottom={5}>
+          회원정보수정
+        </Typography>
+        <Typography variant="BODY2" marginTop={5} marginBottom={5}>
+          회원정보를 수정하는 곳입니다.
+        </Typography>
+        <Typography variant="BODY3" marginTop={5} marginBottom={5}>
+          회색 박스는 변경 불가능합니다.
+        </Typography>
+      </Box>
+      <Container header="필수 정보" headerLine>
+        <EditPageRequire />
+      </Container>
+      <Container header="추가 정보" headerLine>
+        <EditPageAdditional />
+      </Container>
+    </Flex>
+  );
+};
+
+export default EditPage;
