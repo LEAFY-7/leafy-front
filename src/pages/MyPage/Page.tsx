aa14c3f39ade5e2186430747f16941fec7ef1a50
ohtaekwon <ohtk1119@gmail.com>
2023-06-16 20:47:59 +0900

    [MODIFY] 라우터 변경

--- src/pages/MyPage/Page.tsx
+++ src/pages/MyPage/Page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Box from "@components/ui/Box";
+import Flex from "@components/ui/Flex";
+import Typography from "@components/ui/Typograph";
 
 const MyPage = () => {
-  return <div>Page</div>;
+  return (
+    <Flex as="div" direction="column">
+      마이페이지
+    </Flex>
+  );
 };
 
 export default MyPage;
