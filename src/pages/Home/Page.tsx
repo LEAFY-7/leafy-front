02b6ddd9d574fbb68d96e81f2c1d30993379a4a3
3pointdev <motors3996@gmail.com>
2023-06-21 23:40:46 +0900

    [ADD] : 메인페이지 Feed Card 추가

--- src/pages/Home/Page.tsx
+++ src/pages/Home/Page.tsx
@@ -1,36 +1,25 @@
-import React from "react";
-import { css } from "@emotion/react";
+import Card from "@components/Card";
+import styled from "@emotion/styled";
+import { inject, observer } from "mobx-react";
+import Feed from "../../db/feed.json";
 
-import Button from "@components/atoms/Button/rectangle-button";
-import Flex from "@components/atoms/Group/flex";
-/**
- * Page의 관심사 분리 예시 입니다.
- *
- * 최상위 루트로 폴더를 잡고 페이지가 되는 컴포넌트는 Page로 명명하고
- * 같은 관심사의 컴포넌트는 해당 위치로 경로를 잡아주세요.
- *
- */
-
-const Home = () => {
+function Home(props: any) {
+  console.log(props);
   return (
-    <Flex direction="column">
-      <Button size="sm" variant="green">
-        재사용 버튼
-      </Button>
-      <Button size="sm" variant="green_border">
-        재사용 버튼
-      </Button>
-      <Button size="sm" variant="green_border" disabled>
-        재사용 버튼
-      </Button>
-      <Button size="lg" variant="green_border" disabled>
-        재사용 버튼
-      </Button>
-      <Button size="md" variant="green_border" disabled>
-        재사용 버튼
-      </Button>
-    </Flex>
+    <div>
+      <CardWrap>
+        {Feed.data.map((item, key: number) => {
+          return <Card item={item} key={`feed_card_${key}`} />;
+        })}
+      </CardWrap>
+    </div>
   );
-};
+}
 
-export default Home;
+export default inject("mainViewModel")(observer(Home));
+
+const CardWrap = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 16px;
+`;
