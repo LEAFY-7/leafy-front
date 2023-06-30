f4e746237dd9bfecad8b0c3720fe80d56a5e0204
ohtaekwon <ohtk1119@gmail.com>
2023-06-30 21:41:52 +0900

    [REFACTOR] : 로그인 회원가입의 formstate적용한 중복코드 제거

--- src/pages/Auth/SignInForm.tsx
+++ src/pages/Auth/SignInForm.tsx
@@ -1,83 +1,43 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import UserViewModel from '@viewModel/user/user.viewModel';
+import { RiLockPasswordLine } from 'react-icons/ri';
+import { AiOutlineExclamationCircle, AiOutlineMail } from 'react-icons/ai';
+
+import useViewModel, { ViewModelName } from '@hooks/useViewModel';
+import formConfig from '@configs/form.config';
+
 import RectangleButton from '@components/atoms/Button/rectangle-button';
 import Flex from '@components/atoms/Group/flex';
 import TextFiled from '@components/molecules/TextField';
-import useViewModel, { ViewModelName } from '@hooks/useViewModel';
-import { emailRule, krRule, passwordRule } from '@utils/validate';
-import UserViewModel from '@viewModel/user/user.viewModel';
-import { useForm } from 'react-hook-form';
-import { AiOutlineExclamationCircle, AiOutlineMail } from 'react-icons/ai';
-import { RiLockPasswordLine } from 'react-icons/ri';
+import Div from '@components/atoms/Div/default-div';
 
 type FormValues = {
     email: string;
     password: string;
 };
+const DialogLabelButtonType = (<Flex />).type;
 
+const defaultValues: FormValues = {
+    email: '',
+    password: '',
+};
 const SignInForm = () => {
     const userViewModel: UserViewModel = useViewModel(ViewModelName.USER);
     console.log(userViewModel.user);
+
+    console.log(DialogLabelButtonType);
     const {
         register,
         handleSubmit,
-        watch,
-        formState: { errors },
-    } = useForm({
-        defaultValues: {
-            email: '',
-            password: '',
-        },
-    });
+        formState: { errors, isSubmitting, isDirty },
+    } = useForm<FormValues>({ defaultValues });
 
     const onSubmit = async (data: FormValues) => {
+        /**
+         * 로그인 POST 요청
+         */
         console.log(data);
-    };
-
-    const formState = {
-        name: {
-            required: '이름 입력은 필수 입니다.',
-            minLength: {
-                value: 2,
-                message: '최소 2자리 이상 입력하셔야 합니다.',
-            },
-            maxLength: {
-                value: 20,
-                message: '최대 20자리 이하 입력하세야 합니다.',
-            },
-            pattern: {
-                value: krRule,
-                message: '이름은 한글만 작성해주세요.',
-            },
-        },
-        email: {
-            required: '이메일 입력은 필수 입니다.',
-            minLength: {
-                value: 7,
-                message: '최소 7자리 이상 입력하셔야 합니다.',
-            },
-            maxLength: {
-                value: 20,
-                message: '최대 20자리 이하 입력하세야 합니다.',
-            },
-            pattern: {
-                value: emailRule,
-                message: '이메일 형식에 맞지 않습니다',
-            },
-        },
-        password: {
-            required: '비밀번호 입력은 필수 입니다.',
-            minLength: {
-                value: 6,
-                message: '최소 6글자 이상의 비밀번호를 입력해주세요.',
-            },
-            maxLength: {
-                value: 16,
-                message: '16자 이하의 비밀번호만 사용 가능합니다. ',
-            },
-            pattern: {
-                value: passwordRule,
-                message: '비밀번호 형식에 맞지 않습니다.',
-            },
-        },
     };
 
     return (
@@ -91,7 +51,7 @@ const SignInForm = () => {
                     error={!!errors.email}
                     helperText={errors.email?.message}
                     helperIcon={<AiOutlineExclamationCircle />}
-                    {...register('email', formState.email)}
+                    {...register('email', formConfig.signFormState.email)}
                 />
                 <TextFiled
                     hookForm
@@ -101,11 +61,19 @@ const SignInForm = () => {
                     helperText={errors.password?.message}
                     leftIcon={<RiLockPasswordLine />}
                     helperIcon={<AiOutlineExclamationCircle />}
-                    {...register('password', formState.password)}
+                    {...register('password', formConfig.signFormState.password)}
                 />
-                <RectangleButton type="submit" variant="green">
-                    로그인
-                </RectangleButton>
+                <Div id="submit_btn" width={100} size="xs">
+                    <RectangleButton
+                        type="submit"
+                        variant="green"
+                        isBorder
+                        disabled={isSubmitting}
+                        style={{ width: '100%' }}
+                    >
+                        회원가입하기
+                    </RectangleButton>
+                </Div>
             </Flex>
         </form>
     );
--- src/pages/Auth/SignUpForm.tsx
+++ src/pages/Auth/SignUpForm.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import { useDispatch } from 'react-redux';
-import { HiOutlineCheckCircle } from 'react-icons/hi';
 import { AiOutlineExclamationCircle, AiOutlineMail, AiOutlineUserDelete } from 'react-icons/ai';
 import { RiLockPasswordLine } from 'react-icons/ri';
 import { toast } from 'react-toastify';
+import formConfig from '@configs/form.config';
 
-import TextFiled from '@components/molecules/TextField';
-import { emailRule, krRule, passwordRule } from '@utils/validate';
-import userApis from '@api/modules/user.api';
-import RectangleButton from '@components/atoms/Button/rectangle-button';
 import Flex from '@components/atoms/Group/flex';
+import RectangleButton from '@components/atoms/Button/rectangle-button';
+import TextFiled from '@components/molecules/TextField';
+import Div from '@components/atoms/Div/default-div';
 
 type FormValues = {
     displayName: string;
@@ -19,94 +17,27 @@ type FormValues = {
     confirmPassword: string;
 };
 
-const SignUpForm = () => {
-    const dispatch = useDispatch();
+const defaultValues: FormValues = {
+    displayName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+};
 
+const SignUpForm = () => {
     const {
         register,
         handleSubmit,
         watch,
-        formState: { errors, isSubmitting, isDirty, isValid },
-    } = useForm({
-        defaultValues: {
-            displayName: '',
-            email: '',
-            password: '',
-            confirmPassword: '',
-        },
-    });
+        formState: { errors, isSubmitting, isDirty },
+    } = useForm<FormValues>({ defaultValues });
 
-    const formState = {
-        name: {
-            required: '이름 입력은 필수 입니다.',
-            minLength: {
-                value: 2,
-                message: '최소 2자리 이상 입력하셔야 합니다.',
-            },
-            maxLength: {
-                value: 20,
-                message: '최대 20자리 이하 입력하세야 합니다.',
-            },
-            pattern: {
-                value: krRule,
-                message: '이름은 한글만 작성해주세요.',
-            },
-        },
-        email: {
-            required: '이메일 입력은 필수 입니다.',
-            minLength: {
-                value: 7,
-                message: '최소 7자리 이상 입력하셔야 합니다.',
-            },
-            maxLength: {
-                value: 20,
-                message: '최대 20자리 이하 입력하세야 합니다.',
-            },
-            pattern: {
-                value: emailRule,
-                message: '이메일 형식에 맞지 않습니다',
-            },
-        },
-        password: {
-            required: '비밀번호 입력은 필수 입니다.',
-            minLength: {
-                value: 6,
-                message: '최소 6글자 이상의 비밀번호를 입력해주세요.',
-            },
-            maxLength: {
-                value: 16,
-                message: '16자 이하의 비밀번호만 사용 가능합니다. ',
-            },
-            pattern: {
-                value: passwordRule,
-                message: '비밀번호 형식에 맞지 않습니다.',
-            },
-        },
-        confirmPassword: {
-            required: '비밀번호 확인은 필수 입니다.',
-            minLength: {
-                value: 6,
-                message: '최소 6글자 이상의 비밀번호를 입력해주세요.',
-            },
-            maxLength: {
-                value: 16,
-                message: '16자 이하의 비밀번호만 사용 가능합니다. ',
-            },
-            validate: (value: string) => value === watch('password'),
-        },
-    };
+    const onValidate = (value: string) => value === watch('password');
 
     const onSubmit = async (data: FormValues) => {
         /**
-         * 여기에 POST 요청
+         * 회원가입 POST 요청
          */
-
-        // const { response, error } = await userApis.signUp(data);
-
-        // if (response) {
-        //   console.log(response);
-        //   toast.success("회원가입이 성공하였습니다.");
-        // }
         toast.success('회원가입이 성공하였습니다.');
     };
 
@@ -121,7 +52,7 @@ const SignUpForm = () => {
                     error={!!errors.displayName}
                     helperText={errors.displayName?.message}
                     helperIcon={<AiOutlineExclamationCircle />}
-                    {...register('displayName', formState.name)}
+                    {...register('displayName', formConfig.signFormState.name)}
                 />
                 <TextFiled
                     hookForm
@@ -131,7 +62,7 @@ const SignUpForm = () => {
                     error={!!errors.email}
                     helperText={errors.email?.message}
                     helperIcon={<AiOutlineExclamationCircle />}
-                    {...register('email', formState.email)}
+                    {...register('email', formConfig.signFormState.email)}
                 />
                 <TextFiled
                     hookForm
@@ -141,7 +72,7 @@ const SignUpForm = () => {
                     helperText={errors.password?.message}
                     leftIcon={<RiLockPasswordLine />}
                     helperIcon={<AiOutlineExclamationCircle />}
-                    {...register('password', formState.password)}
+                    {...register('password', formConfig.signFormState.password)}
                 />
                 <TextFiled
                     hookForm
@@ -151,11 +82,19 @@ const SignUpForm = () => {
                     helperText={errors.confirmPassword?.message || '비밀번호가 일치하지 않습니다.'}
                     leftIcon={<RiLockPasswordLine />}
                     helperIcon={<AiOutlineExclamationCircle />}
-                    {...register('confirmPassword', formState.confirmPassword)}
+                    {...register('confirmPassword', formConfig.signFormState.confirmPassword(onValidate))}
                 />
-                <RectangleButton type="submit" variant="green">
-                    확인
-                </RectangleButton>
+                <Div id="submit_btn" width={100} size="xs">
+                    <RectangleButton
+                        type="submit"
+                        variant="green"
+                        isBorder
+                        disabled={isSubmitting}
+                        style={{ width: '100%' }}
+                    >
+                        로그인하기
+                    </RectangleButton>
+                </Div>
             </Flex>
         </form>
     );
