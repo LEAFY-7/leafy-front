62fde3e9628e93d3712c5be7b4cfe10569ba0785
ohtaekwon <ohtk1119@gmail.com>
2023-07-05 02:07:23 +0900

    [MODIFY] 대문자 파일 삭제

--- scripts/after_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source /home/ec2-user/leafy-front/.env.production
-
-export REACT_APP_MODE=$REACT_APP_MODE
-export REACT_APP_BASE_URL=$REACT_APP_BASE_URL
-
-# export NODE_OPTIONS=--max_old_space_size=4096
