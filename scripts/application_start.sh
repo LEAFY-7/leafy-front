1c8993c54546fa7b45b24d12f9aefbb040c1c5b0
ohtaekwon <ohtk1119@gmail.com>
2023-07-05 23:37:41 +0900

    [FIX] sudo, screen 추가 포트 변경

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,23 +2,15 @@
 
 cd /home/ec2-user/leafy-front
 
-echo "" >> debug.log
-
 npm install
-
-echo "installed" >> debug.log
 
 npm run clean
 
-echo "cleaned" >> debug.log
-
 npm run build
 
-echo "build" >> debug.log
-
-nohup npm run serve & > /dev/null 2> /dev/null < /dev/null &
-
-echo "served" >> debug.log
-
 aws s3 sync build/ s3://bucket-leafy
+
+screen
+
+sudo nohup npm run serve & > /dev/null 2> /dev/null < /dev/null &
 
