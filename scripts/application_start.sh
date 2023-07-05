2740373f0f64bddeb2e51e28a10be6e7f64e068f
hjin <125787016+paramil@users.noreply.github.com>
2023-07-05 20:19:42 +0900

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,13 +2,23 @@
 
 cd /home/ec2-user/leafy-front
 
+echo "" >> debug.log
+
 npm install
+
+echo "installed" >> debug.log
 
 npm run clean
 
+echo "cleaned" >> debug.log
+
 npm run build
 
-nohup npm run serve &
+echo "build" >> debug.log
+
+nohup npm run serve & > /dev/null 2> /dev/null < /dev/null &
+
+echo "served" >> debug.log
 
 aws s3 sync build/ s3://bucket-leafy
 
