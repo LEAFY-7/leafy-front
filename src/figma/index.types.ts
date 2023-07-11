86040283f3042ad0b4e141c0696b495eb1324cc4
ohtaekwon <ohtk1119@gmail.com>
2023-07-11 10:17:14 +0900

    [ADD] 토큰에 따른 색상 타입 정의

---
+++ src/figma/index.types.ts
@@ -0,0 +1,37 @@
+import global from './global.json';
+
+export const TokenGlobal = {
+    green: global.green,
+    lgreen: global.lgreen,
+    white: global.white,
+    lgrey: global.lgrey,
+    grey: global.grey,
+    black: global.black,
+    red: global.red,
+    yellow: global.yellow,
+    blossom: global.blossom,
+    'small-shadow': global['small-shadow'],
+    'big-shadow': global['big-shadow'],
+    shadow: global.shadow,
+    show: global.show,
+    hide: global.hide,
+} as const;
+
+export const TokenGlobalColorList = [
+    TokenGlobal.green,
+    TokenGlobal.lgreen,
+    TokenGlobal.black,
+    TokenGlobal.blossom,
+    TokenGlobal.green,
+    TokenGlobal.grey,
+    TokenGlobal.red,
+    TokenGlobal.yellow,
+    TokenGlobal.white,
+] as const;
+
+export type TokeGlobalType = typeof TokenGlobal;
+export type TokenGlobalOnlyColor = Omit<
+    TokeGlobalType,
+    'big-shadow' | 'shadow' | 'small-shadow' | 'show' | 'hide'
+>;
+export type TokenGlobalOnlyColorDescription = 'primary' | 'secondary' | 'basic' | 'semantic' | 'chat bg';
