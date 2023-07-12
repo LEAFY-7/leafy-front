a1f0e7cf94fe952d0019c84e78295944b9292571
ohtaekwon <ohtk1119@gmail.com>
2023-07-12 19:31:43 +0900

    [STYLE] 템플릿 높이 값 수정 및 TRI 템플릿 삭제

--- src/components/templates/mono-template.tsx
+++ src/components/templates/mono-template.tsx
@@ -1,13 +1,47 @@
-import { ReactNode } from 'react';
+/** @jsxImportSource @emotion/react */
+import type { CSSProperties, ReactNode } from 'react';
+import { css } from '@emotion/react';
 import Box from 'components/atoms/Box/default-box';
 
 interface Props {
     mainSection: ReactNode;
+    width?: CSSProperties['width'];
+    height?: CSSProperties['height'];
+    margin?: CSSProperties['margin'];
+    padding?: CSSProperties['padding'];
+    display?: 'none' | 'flex' | 'visible' | 'inline-flex';
+    justifyContent?: CSSProperties['justifyContent'];
+    alignItems?: CSSProperties['alignItems'];
+    direction?: 'row' | 'column';
 }
 
-const MonoTemplate = ({ mainSection }: Props) => {
+const MonoTemplate = ({
+    mainSection,
+    width = '100%',
+    height = '100vh',
+    display = 'flex',
+    alignItems = 'center',
+    justifyContent = 'center',
+    margin,
+    padding = '3rem',
+    children,
+    ...rest
+}: React.PropsWithChildren<Props>) => {
     return (
-        <Box as="main" width={100} height={100} minHeight="100vh" padding="3rem" radius={0}>
+        <Box
+            as="main"
+            width={width}
+            height={height}
+            minHeight="100vh"
+            display={display}
+            justifyContent={justifyContent}
+            alignItems={alignItems}
+            padding={padding}
+            margin={margin}
+            radius={0}
+            direction="column"
+            {...rest}
+        >
             {mainSection}
         </Box>
     );
--- src/components/templates/template.style.ts
+++ src/components/templates/template.style.ts
@@ -25,47 +25,5 @@ const variantStyles = {
             rightHeight: 100,
         },
     },
-    tri: {
-        '1/1/1': {
-            leftWidth: 33.3,
-            middleWidth: 33.3,
-            rightWidth: 33.3,
-            leftHeight: 100,
-            middleHeight: 100,
-            rightHeight: 100,
-        },
-        '1/2/1': {
-            leftWidth: 25,
-            middleWidth: 50,
-            rightWidth: 25,
-            leftHeight: 100,
-            middleHeight: 100,
-            rightHeight: 100,
-        },
-        '1/3/1': {
-            leftWidth: 20,
-            middleWidth: 60,
-            rightWidth: 20,
-            leftHeight: 100,
-            middleHeight: 100,
-            rightHeight: 100,
-        },
-        '1/4/1': {
-            leftWidth: 16.6,
-            middleWidth: 66.4,
-            rightWidth: 16.6,
-            leftHeight: 100,
-            middleHeight: 100,
-            rightHeight: 100,
-        },
-        '2/4/1': {
-            leftWidth: 28,
-            middleWidth: 56,
-            rightWidth: 16,
-            leftHeight: 100,
-            middleHeight: 100,
-            rightHeight: 100,
-        },
-    },
 };
 export default { variantStyles };
