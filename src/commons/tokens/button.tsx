a4e7cd540b4b5de9c63f1b3eaa26d804dcd662f6
Sleepingoff <justjh30@gmail.com>
2023-07-19 21:54:04 +0900

    [MODIFY] 버튼 사이즈 수정

--- src/commons/tokens/button.tsx
+++ src/commons/tokens/button.tsx
@@ -2,8 +2,6 @@
 import React from 'react';
 import { buttonSize, buttonState, buttonStep, buttonText } from './button.style';
 import { RxArrowTopRight } from 'react-icons/rx';
-import { TokenButtonStyle } from './button.types';
-import { css } from '@emotion/react';
 
 interface Props {
     id?: string;
@@ -26,7 +24,7 @@ export const Button = ({
     showIcon = true,
     leftIcon = <RxArrowTopRight />,
 }: React.PropsWithChildren<Props>) => {
-    const sizeCss = buttonSize(size).value.styles;
+    const sizeCss = buttonSize(size).size.styles;
     const stepCss = buttonStep[state][variant].styles;
     const stateCss = buttonState(state, variant).state;
     const textCss = buttonText(showText && 'text', variant).state.styles;
