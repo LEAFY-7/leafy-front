4db500461bb6339fdeab94910d1d5d79a4df25b8
Sleepingoff <justjh30@gmail.com>
2023-07-24 02:23:37 +0900

    [ADD] 버튼 컴포넌트으로 이동

---
+++ src/components/atoms/Button/button.tsx
@@ -0,0 +1,39 @@
+/** @jsxImportSource @emotion/react */
+import React from 'react';
+import { buttonSize, buttonState, buttonStep, buttonText } from './buttons.style';
+import { RxArrowTopRight } from 'react-icons/rx';
+
+interface Props {
+    id?: string;
+    size: 'xl' | 'l' | 's';
+    variant: 'primary' | 'secondary' | 'basic';
+    state?: 'default' | 'disabled' | 'hover' | 'focus' | 'pressed';
+    text: string;
+    leftIcon?: React.ReactNode;
+    showIcon: boolean;
+    showText: boolean;
+}
+
+export const Button = ({
+    id,
+    variant = 'secondary',
+    size = 'xl',
+    state = 'default',
+    text = 'click',
+    showText = true,
+    showIcon = true,
+    leftIcon = <RxArrowTopRight />,
+}: React.PropsWithChildren<Props>) => {
+    const sizeCss = buttonSize(size).size.styles;
+    const stepCss = buttonStep[state][variant].styles;
+    const stateCss = buttonState(state, variant).state;
+    const textCss = buttonText('text', variant).state.styles;
+    return (
+        <button css={[sizeCss, stepCss, stateCss, textCss]}>
+            {showText && text}
+            {showIcon && leftIcon}
+        </button>
+    );
+};
+
+export default Button;
---
+++ src/components/atoms/Button/button.types.ts
@@ -0,0 +1,49 @@
+import { Properties } from "@tokens-studio/types";
+import button from "../../../commons/tokens/button.json";
+
+const TokenButton = { 
+    size : button.size,
+    default : button.default,
+    disabled : button.disabled,
+    pressed : button.pressed,
+    text : button.text,
+    hover : button.hover,
+    focus : button.focus
+};
+
+ const TokenButtonSize = {
+    xl : TokenButton.size["sys-button-xl"],
+    l : TokenButton.size["sys-button-l"],
+    s : TokenButton.size["sys-button-s"]
+};
+
+type size = "xl" | "l" | "s";
+
+export const TokenButtonSizeList = ( size:size ) =>{
+    return{
+        width: TokenButtonSize[size][Properties.width].value,
+        height: TokenButtonSize[size][Properties.height].value,
+        vPadding: TokenButtonSize[size][Properties.verticalPadding].value,
+        hPadding: TokenButtonSize[size][Properties.horizontalPadding].value,
+        borderRadius: TokenButtonSize[size][Properties.borderRadius].value,
+        typography: TokenButtonSize[size][Properties.typography]
+    }   
+}
+
+type states = "default" | "disabled"| "pressed" | "text";
+
+type steps = "primary" | "secondary" | "basic" | "basic-disabled";
+
+//state에서 step찾기
+export const TokenButtonState = (state:states, step:steps) => {
+    return TokenButton[state][`sys-button-${step}`];
+}
+export const TokenButtonStyle = (state:states, step:steps) => {
+    const token = TokenButtonState(state,  step);
+    return {
+        backgroundColor : token.fill.value,
+        borderColor : token.border.color.value,
+        borderWidth : token.border.width.value,
+        borderStyle : token.border.style.value
+    }
+};
