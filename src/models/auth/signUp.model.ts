1a744679a1ec9fa97c79ce85bcf2c94d104c68a6
ohtaekwon <ohtk1119@gmail.com>
2023-07-25 19:50:05 +0900

    [UPDATE] auth model 필요에 맞게 변경

--- src/models/auth/signUp.model.ts
+++ src/models/auth/signUp.model.ts
@@ -1,6 +1,17 @@
 import { SignInModel } from './signIn.model';
 
 export class SignUphModel extends SignInModel {
-    public displayName: string = '';
+    public name: string = '';
+    public nickName: string = '';
     public confirmPassword: string = '';
+    public phone: string = '';
+    public birthDay: string = '';
+    public zoneCode: string = '';
+    public address: string = '';
+    public jibunAddress: string = '';
+    public roadAddress: string = '';
+    public addressDetail: string = '';
+    public gender: 'MALE' | 'FEMALE' = 'MALE';
+    public simpleIntroduction: string = '';
+    public addressIsHide: boolean = true;
 }
