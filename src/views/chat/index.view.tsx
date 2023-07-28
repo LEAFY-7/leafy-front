728da84a3bfc82db1fdc14b8b760c15747ad9f2e
ohtaekwon <ohtk1119@gmail.com>
2023-07-28 13:26:25 +0900

    [FIX] TokenModule 에러 수정

--- src/views/chat/index.view.tsx
+++ src/views/chat/index.view.tsx
@@ -1,9 +1,17 @@
+import io from 'socket.io-client';
 import PageContainer from 'components/templates/page-container';
 
-/**
- * 채팅
- */
+let socket;
+
 const ChatView = () => {
+    // const ENDPOINT = 'http://localhost:5000/chat';
+    // React.useEffect(() => {
+    //     socket = io(ENDPOINT);
+    //     console.log(socket);
+
+    //     socket.emit('join', { name, room });
+    // }, [ENDPOINT]);
+
     return <PageContainer>ChatView</PageContainer>;
 };
 
