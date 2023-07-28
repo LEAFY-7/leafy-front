35799e906e1ef6b89bf13bec5868a00c7b4f7832
ohtaekwon <ohtk1119@gmail.com>
2023-07-28 12:52:11 +0900

    [ADD] 토큰 모듈 구현

--- src/modules/token.module.ts
+++ src/modules/token.module.ts
@@ -1,84 +1,64 @@
-// class TokenModule {
-//     public save(token: string, userAuth: string) {
-//         window.localStorage.setItem('token', token);
-//         window.localStorage.setItem('userAuth', userAuth);
-//     }
+import { AuthDto } from 'dto/auth/auth.dto';
 
-//     public get() {
-//         return {
-//             token: window.localStorage.getItem('token'),
-//             userAuth: window.localStorage.getItem('userAuth'),
-//         };
-//     }
-
-//     public destroy() {}
-// }
-
-// export default new TokenModule();
-
+enum LocalStorageKey {
+    USER_AUTH = 'userAuth',
+    TOKEN = 'token',
+}
 enum ErrorMessage {
-    SAVE = '로컬 스토리지에 저장하는 도중 에러가 발생했습니다.',
     GET = '로컬 스토리지에서 가져오는 도중 에러가 발생했습니다.',
+    SAVE = '로컬 스토리지에 저장하는 도중 에러가 발생했습니다.',
     REMOVE = '로컬 스토리지에서 삭제하는 도중 에러가 발생했습니다.',
 }
+type ErrorMessageKey = keyof typeof ErrorMessage;
 
-export default class TokenModule {
-    private static instance: TokenModule;
-    private storageKey: string;
-    private saveErrorMessage: string;
-    private getErrorMessage: string;
-    private removeErrorMessage: string;
+interface AuthToken {
+    auth: Omit<AuthDto, 'userId'>;
+}
 
-    constructor(storageKey: string) {
-        this.storageKey = storageKey;
-        this.saveErrorMessage = ErrorMessage.SAVE;
-        this.getErrorMessage = ErrorMessage.GET;
-        this.removeErrorMessage = ErrorMessage.REMOVE;
-    }
+class TokenModule {
+    private tokenKey = LocalStorageKey.TOKEN; // jwt 토큰
+    private userAuthKey = LocalStorageKey.USER_AUTH; // user 상태
 
-    private static instancesMap: Map<string, TokenModule> = new Map();
-
-    public static getInstance(storageKey: string): TokenModule {
-        if (this.instancesMap.has(storageKey)) {
-            return this.instancesMap.get(storageKey)!;
-        } else {
-            const newInstance = new this(storageKey);
-            this.instancesMap.set(storageKey, newInstance);
-            return newInstance;
-        }
-    }
-
-    public saveToken = (token) => {
+    public save = ({ auth: { token, userAuth } }: AuthToken) => {
         try {
-            localStorage.setItem(this.storageKey, JSON.stringify(token));
+            window.localStorage.setItem(this.tokenKey, JSON.stringify(token));
+            window.localStorage.setItem(this.userAuthKey, JSON.stringify(userAuth));
             return true;
         } catch (error) {
-            this.handleError(this.saveErrorMessage, error);
+            this.handleError('SAVE', error);
             throw error;
         }
     };
 
-    public getToken = () => {
+    public get = () => {
         try {
-            const tokenString = localStorage.getItem(this.storageKey);
-            return JSON.parse(tokenString);
+            const token = window.localStorage.getItem(this.tokenKey);
+            const userAuth = window.localStorage.getItem(this.userAuthKey);
+            return {
+                token: token ? JSON.parse(token) : null,
+                userAuth: token ? JSON.parse(userAuth) : null,
+            };
         } catch (error) {
-            this.handleError(this.getErrorMessage, error);
+            this.handleError('GET', error);
             throw error;
         }
     };
 
-    public removeToken() {
+    public remove = () => {
         try {
-            localStorage.removeItem(this.storageKey);
+            window.localStorage.removeItem(this.tokenKey);
+            window.localStorage.removeItem(this.userAuthKey);
             return true;
         } catch (error) {
-            this.handleError(this.removeErrorMessage, error);
+            this.handleError('REMOVE', error);
             throw error;
         }
-    }
+    };
 
-    private handleError = (message, error) => {
-        console.error(message, error);
+    private handleError = (message: ErrorMessageKey, error: Error) => {
+        console.error(ErrorMessage[message], error);
     };
 }
+
+const tokenModule = new TokenModule();
+export default tokenModule;
