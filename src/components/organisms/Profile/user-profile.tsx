586568f0cb6c6523713c9c00ee68cce50a0faa2d
ohtaekwon <ohtk1119@gmail.com>
2023-07-30 14:28:01 +0900

    [FIX] 없는 경로 에러

--- src/components/organisms/Profile/user-profile.tsx
+++ src/components/organisms/Profile/user-profile.tsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
 import LazyImage from 'components/atoms/LazyImage/default-image';
+import { UserDto } from 'dto/user/user.dto';
 
 interface IProps {
     data: UserDto;
