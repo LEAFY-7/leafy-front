d3a36ac2b047e063cfd58aa6c1464def80d38f26
ohtaekwon <ohtk1119@gmail.com>
2023-08-09 20:29:43 +0900

    [ADD] 소켓 config 추가

---
+++ src/configs/socket.config.ts
@@ -0,0 +1,8 @@
+class SocketConfigs {
+    public join = 'join';
+    public send = 'send';
+    public messageHistory = 'messageHistory';
+    public receiveMessage = 'receiveMessage';
+}
+const socketConfigs = new SocketConfigs();
+export default socketConfigs;
