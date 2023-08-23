26a5b52f1622917233196cc77ae672a6eeabfd9b
ohtaekwon <ohtk1119@gmail.com>
2023-08-23 22:06:20 +0900

    [MODIFY] GETME요청 주석처리

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
 
     React.useEffect(() => {
         defaultViewModel.handleThemeMode();
-        defaultViewModel.getMe();
+        // defaultViewModel.getMe();
     }, []);
 
     console.log(defaultViewModel.me);
