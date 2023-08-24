aa3997298e43bea73d0bf17ef528a53246031552
ohtaekwon <ohtk1119@gmail.com>
2023-08-24 18:38:02 +0900

    [UPDATE] userId 추가 및 userAuth 타입 지정

--- src/dto/auth/auth.dto.ts
+++ src/dto/auth/auth.dto.ts
@@ -1,11 +1,12 @@
 import { Expose } from 'class-transformer';
+import type { AllowedRole } from 'components/organisms/routes/index.types';
 
 export class AuthDto {
     @Expose({ name: 'token' })
     public readonly token: string = '';
 
     @Expose({ name: 'userAuth' })
-    public readonly userAuth: string = '';
+    public readonly userAuth: AllowedRole = 'NORMAL';
 
     @Expose({ name: 'userId' })
     public readonly userId: number = 0;
