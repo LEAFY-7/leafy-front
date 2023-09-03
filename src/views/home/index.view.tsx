a30fefff066cbf1145da2d29df048086f53e2dfb
Sleepingoff <justjh30@gmail.com>
2023-09-03 15:44:01 +0900

    [ADD] appspec.yml 파일 추가

--- src/views/home/index.view.tsx
+++ src/views/home/index.view.tsx
@@ -8,6 +8,8 @@ import { useEffect } from 'react';
 import MainViewModel from 'viewModel/main/main.viewModel';
 import SearchViewModel from 'viewModel/search/search.viewModel';
 import Button from 'components/atoms/Button/button';
+import Typography from 'components/atoms/Typograph/default-typography';
+import { theme } from 'configs/ui.config';
 
 /**
  * 메인페이지
@@ -22,16 +24,15 @@ const HomeView = () => {
 
     return (
         <PageContainer>
-            <CardWrap>
-                {mainViewModel.feedList.map((item: FeedDto, key: number) => {
-                    return <Card item={item} key={`feed_card_${key}`} />;
-                })}
-            </CardWrap>
-            <CardWrap>
-                {mainViewModel.feedList.map((item: FeedDto, key: number) => {
-                    return <Card item={item} key={`feed_card_${key}`} />;
-                })}
-            </CardWrap>
+            <Typography variant="H1" color="primary" textAlign="center">
+                식집사들의 커뮤니티
+            </Typography>
+            {/* 카드 리스트 */}
+            {!mainViewModel.me.name && (
+                <Typography variant="H1" color="primary" textAlign="center">
+                    로그인 후 더 많은 콘텐츠를 즐겨보세요!
+                </Typography>
+            )}
         </PageContainer>
     );
 };
