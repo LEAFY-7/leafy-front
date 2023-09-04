ff2fc57425f78819c9933c68fb0e2fd1d511566f
ohtaekwon <ohtk1119@gmail.com>
2023-09-04 17:24:52 +0900

    [UPDATE] Script pm2로 변경

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -8,6 +8,6 @@ npm install
 npm run clean
 npm run build
 aws s3 sync build/ s3://bucket-leafy
-screen -dmS leafy
-screen -S leafy -X stuff 'sudo nohup npm run serve & > /dev/null 2> /dev/null < /dev/null &\n'
+npx pm2 start "npm run serve" --name "leafyer-front"
+
 
