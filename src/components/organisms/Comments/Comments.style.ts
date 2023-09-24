306a0bfe0b68d62111061cb69fe195912f65e550
Sleepingoff <justjh30@gmail.com>
2023-09-24 13:37:46 +0900

    [ADD] 댓글 컴포넌트 작성

---
+++ src/components/organisms/Comments/Comments.style.ts
@@ -0,0 +1,28 @@
+import styled from '@emotion/styled';
+
+const Comment = {
+    Wrap: styled.div`
+        width: 100%;
+        background: #fafafa;
+        border-radius: 8px;
+        padding: 8px;
+        display: flex;
+        flex-direction: column;
+        gap: 16px;
+    `,
+    Button: styled.button`
+        width: 40px;
+        height: 40px;
+        border-radius: 8px;
+        transition: all 0.4s ease;
+
+        border: 2px solid #d9d9d9;
+
+        &:hover {
+            background: #d9d9d9;
+        }
+    `,
+};
+
+
+export default Comment;
