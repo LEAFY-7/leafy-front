7f4c04419d08939942baa2c7bd178161945de171
ohtaekwon <ohtk1119@gmail.com>
2023-10-09 15:48:51 +0900

    [UPDATE] 이미지 임시 STATE 추가

--- src/viewModel/user/user.viewModel.ts
+++ src/viewModel/user/user.viewModel.ts
@@ -1,5 +1,5 @@
 import { action, makeObservable, observable, runInAction } from 'mobx';
-import { AxiosError, AxiosResponse } from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
 import { plainToInstance } from 'class-transformer';
 
 import { SignUphModel } from 'models/auth/signUp.model';
@@ -10,6 +10,9 @@ import FeedActivityDto from 'dto/my/feedActivity.dto';
 import MyPageDto from 'dto/my/mypage.dto';
 import TotalCountDto from 'dto/my/totalCount.dto';
 import { UserDto } from 'dto/user/user.dto';
+import tokenModule from 'modules/token.module';
+import { Alert } from 'modules/alert.module';
+import { error } from 'console';
 
 interface IProps {}
 
@@ -18,6 +21,7 @@ export default class UserViewModel extends DefaultViewModel {
     public totalCount: TotalCountDto = new TotalCountDto();
     public chartList: FeedActivityDto[] = [new FeedActivityDto()];
     public userInformation: SignUphModel = new SignUphModel();
+    public myImage: any = null;
 
     constructor(props: IProps) {
         super(props);
@@ -27,6 +31,7 @@ export default class UserViewModel extends DefaultViewModel {
             userInformation: observable,
             totalCount: observable,
             chartList: observable,
+            myImage: observable,
 
             getUser: action,
             getMyPage: action,
@@ -57,6 +62,114 @@ export default class UserViewModel extends DefaultViewModel {
             .catch((error: AxiosError) => {
                 console.log('error : ', error);
                 return false;
+            });
+    };
+
+    getPageNationOfFollow = (event: React.ChangeEvent<HTMLButtonElement>) => {
+        const { id, name } = event.currentTarget.dataset;
+
+        // switch (name){
+        //     case
+        // }
+    };
+
+    /**
+     * 이미지 업로드 관련 API 요청 로직
+     */
+
+    // 수정 - 임시
+    postMyImage = (imageFile: FormData) => {
+        axios
+            .post('http://www.leafyer.com:8080/api/v1/users/image', imageFile, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    Authorization: tokenModule.get().leafyer.token,
+                },
+            })
+            .then((result: AxiosResponse<any>) => {
+                console.log(result);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
+
+    // 수정 이전
+    updateMyImage = (imageFile: File) => {
+        this.api
+            .post(ServerType.API, '/v1/users/image', imageFile)
+            .then((result: AxiosResponse<any>) => {
+                console.log(result);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+
+    updateMyBackGroundImg = () => {
+        // this.api.post()
+    };
+    updateMyInformation = () => {};
+
+    // DELETE로 요청했는데, POST로 보내짐...
+    deleteMyImage = () => {
+        this.api
+            .delete(ServerType.API, '/v1/users/image')
+            .then((result: AxiosResponse<any>) => {
+                console.log(result);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+    deleteMyBackgroundImage = () => {
+        this.api
+            .delete(ServerType.API, '/v1/users/background-image')
+            .then((result: AxiosResponse<any>) => {
+                console.log(result);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+
+    /**
+     * 설정 관련 API 요청
+     *
+     * 1. 회원 공개 - 200, 204, 500
+     * 2. 댓글 알람
+     * 3. 전체 알람 비공개
+     */
+
+    patchMemberDisclosure = () => {
+        this.api
+            .patch(ServerType.API, '/v1/users/hide')
+            .then((result: AxiosResponse<any>) => {
+                console.log(result);
+            })
+            .catch((error) => {
+                Alert.alert('요청에 실패하였습니다. 다시 시도해주세요.');
+            });
+    };
+
+    patchReviewAlarm = () => {
+        this.api
+            .patch(ServerType.API, '/v1/users/comment-notification')
+            .then((result: AxiosResponse<any>) => {
+                console.log(result);
+            })
+            .catch((error) => {
+                Alert.alert('요청에 실패하였습니다. 다시 시도해주세요.');
+            });
+    };
+    patchAllAlarm = () => {
+        this.api
+            .patch(ServerType.API, '/v1/users/all-notification')
+            .then((result: AxiosResponse<any>) => {
+                console.log(result);
+            })
+            .catch((error) => {
+                Alert.alert('요청에 실패하였습니다. 다시 시도해주세요.');
             });
     };
 }
